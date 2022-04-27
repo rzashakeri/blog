4396f374baf9bf816129c54f737ea53e56bcf77e
rezashakeri <rezashakeri2648@gmail.com>
2022-04-28 04:10:18 +0430

    complete article model

--- article/apps.py
+++ article/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ArticleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'article'
+    verbose_name = 'article'
--- article/models.py
+++ article/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from user_management.models import User
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, verbose_name='title')
+    short_description = models.CharField(max_length=400, verbose_name='short description')
+    description = models.TextField(verbose_name='description')
+    image = models.ImageField(upload_to='article')
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='author')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='create date')
+    slug = models.SlugField(max_length=400, unique=True, verbose_name='url slug')
+    is_active = models.BooleanField(verbose_name='is active ?')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
