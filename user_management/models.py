cc0770002f099171eb0b9eac19e911dc4edf0d66
rezashakeri <rezashakeri2648@gmail.com>
2022-04-28 02:14:39 +0430

    customize user model

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# customize user model
+class User(AbstractUser):
+    mobile = models.CharField(max_length=100, null=True, blank=True, verbose_name='mobile')
+    avatar = models.ImageField()
+    email_active_code = models.CharField(max_length=300, verbose_name='activation code')
+
+    def __str__(self):
+        if self.first_name != '' and self.last_name = '':
+            return self.get_full_name()
+        return self.email
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
