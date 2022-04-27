c976966f99ebcf5b9588efb21f186fe519cad5fc
rezashakeri <rezashakeri2648@gmail.com>
2022-04-28 03:34:02 +0430

    edit apps.py and add verbose name and add model to admin user management

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+
+admin.site.register(models.User, UserAdmin)
