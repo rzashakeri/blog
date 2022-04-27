e37871a57abb92197ee0f8e45f5c5207d35d1318
rezashakeri <rezashakeri2648@gmail.com>
2022-04-28 01:58:16 +0430

    add migration and create database

--- blog/settings.py
+++ blog/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-"""blog URL Configuration
+"""""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
