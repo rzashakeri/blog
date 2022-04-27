2af6f43df452fecac4e7ef4a84ff43cb0097d242
rezashakeri <rezashakeri2648@gmail.com>
2022-04-28 02:28:43 +0430

    configure user model and upload path

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_management'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user_management.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
--- user_management/models.py
+++ user_management/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import AbstractUser
 # customize user model
 class User(AbstractUser):
     mobile = models.CharField(max_length=100, null=True, blank=True, verbose_name='mobile')
-    avatar = models.ImageField()
+    avatar = models.ImageField(upload_to='avatars/')
     email_active_code = models.CharField(max_length=300, verbose_name='activation code')
 
     def __str__(self):
-        if self.first_name != '' and self.last_name = '':
+        if self.first_name != '' and self.last_name != '':
             return self.get_full_name()
         return self.email
 
