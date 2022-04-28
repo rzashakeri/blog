f96cf8f3446333ac09716a2a408c1f32a95fd22f
rezashakeri <rezashakeri2648@gmail.com>
2022-04-29 02:11:21 +0430

    add intro section

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'shared/_layout.html'
+
+
+def header_component(request):
+    return render(request, 'shared/header_component.html')
