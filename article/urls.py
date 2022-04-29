743b0566fbfe4c527fbcb16c59c1c107d88abc16
rezashakeri <rezashakeri2648@gmail.com>
2022-04-30 00:48:22 +0430

    completion category template

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:slug>', views.SingleArticle.as_view(), name='single_article')
+    path('<slug:slug>', views.SingleArticle.as_view(), name='single_article'),
+    path('category/<slug:category>', views.CategoryArticle.as_view(), name='category_article')
 ]
--- article/views.py
+++ article/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
-from article.models import Article
+from article.models import Article, ArticleCategory
 
 
 class SingleArticle(DetailView):
     template_name = 'article/single_article.html'
     model = Article
     context_object_name = 'single_article'
+
+
+class CategoryArticle(ListView):
+    template_name = 'article/category.html'
+    model = Article
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        query = super(CategoryArticle, self).get_queryset()
+        category_name = self.kwargs.get('category')
+        query = query.filter(is_active=True, category__name=category_name)
+        return query
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryArticle, self).get_context_data()
+        context['category_name'] = self.kwargs.get('category')
+        return context
