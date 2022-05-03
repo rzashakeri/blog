5e54e3572e62d7a5850f1baf899670ad70a46014
rezashakeri <rezashakeri2648@gmail.com>
2022-05-04 01:00:31 +0430

    add recommendation section and style

--- article/views.py
+++ article/views.py
@@ -9,6 +9,14 @@ class SingleArticle(DetailView):
     model = Article
     context_object_name = 'single_article'
 
+    def get_context_data(self, **kwargs):
+        context = super(SingleArticle, self).get_context_data()
+        slug = self.kwargs.get('slug')
+        article = Article.objects.filter(slug__iexact=slug).first()
+        categories = article.category.prefetch_related('article_set').all()
+        context['categories'] = categories
+        return context
+
 
 class CategoryArticle(ListView):
     template_name = 'article/category.html'
